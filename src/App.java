f04618f4f3d9a8f4926a4eb5d3d44fee47aee650
YaelDev <jorge.yael333@gmail.com>
2025-11-29 09:34:37 -0600

    Agregando busqueda binaria

--- src/App.java
+++ src/App.java
@@ -4,7 +4,19 @@
 public class App {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        App.runBinarySearchExample();
+    }
+
+
+    private static void runBinarySearchExample() {
+        search.BinarySearch binarySearch = new search.BinarySearch();
+
+        int foundIndex = binarySearch.searchInList(2);
+        System.out.println("Item found at index: " + foundIndex);
+
+        // Suppose you have a sorted list of 128 names, and you’re searching 
+        // through it using binary search. What’s the maximum number of 
+        // steps it would take? = 7 steps (2^7 = 128)
     }
 
 }
--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -5,21 +5,32 @@ package search;
 
 public class BinarySearch {
 
-    private int[] list = {};
+    // maximo 5 iteraciones
+    private int[] list = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
 
     public int searchInList(int item){
         int low = 0;
         int high = list.length - 1;
+        int iterationsCount = 0;
 
-        int mid = (low + high) / 2;
-        Integer guess = list[mid];
+        while (low <= high) {
+            iterationsCount ++;
+            int mid = (low + high) / 2;
+            int guess = list[mid];
 
-        if(guess < item){
-            low = mid + 1;
+            if(guess == item){
+                System.out.println("Iterations count: " + iterationsCount);
+                return mid;
+            }else if(guess < item){     
+                low = mid + 1;
+            }else {
+                high = mid - 1;
+            }
         }
 
-        return 0;
+        return -1;
     }
+
 
 }
